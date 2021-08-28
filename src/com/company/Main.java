5b03881163172bdc196ff287d7af053afd258eb3
Almanbet Totoev <almanbet.totoev@mail.ru>
2021-08-28 12:02:14 +0600

    Lesson53

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,10 @@
 package com.company;
 
+import java.util.Set;
+import java.util.TreeSet;
+
 public class Main {
     public static void main(String[] args) {
-
+        Set<String> set = new TreeSet<>();
     }
 }
