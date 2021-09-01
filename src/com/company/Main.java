a8c49824cc9fa7d5e082d6a05c404020d7ad5125
Almanbet Totoev <almanbet.totoev@mail.ru>
2021-09-01 23:47:34 +0600

    Lesson56

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,22 @@
 package com.company;
 
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-
+        List<Integer> arrayList = new ArrayList<>();
+        arrayList.add(1);
+        arrayList.add(1);
+        arrayList.add(2);
+        System.out.println(arrayList);
+        System.out.println(isNeighbors(arrayList));
+    }
+    public static boolean isNeighbors(List<Integer> list){
+        for (int i = 1; i < list.size(); i++) {
+            if(list.get(i).equals(list.get(i-1))){
+                return true;
+            }
+        }
+        return false;
     }
 }
